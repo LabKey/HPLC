08f37ae3d1c14d1b729b963cd02869dc60d2518d
Nick Arnold <nicka@labkey.com>
2016-02-09 14:25:09 -0800

    Incorporate search metadata in data query.xml

--- resources/assay/rawHPLC/queries/SearchHPLCData.sql
+++ resources/assay/rawHPLC/queries/SearchHPLCData.sql
@@ -1,3 +1,4 @@
+-- This query is used to define the search criteria as used for the rawHPLC initial view
 SELECT
 Data.RowId,
 COALESCE(Data.Name, '') || COALESCE(Data.Run.RunIdentifier, '') || COALESCE(Data.Run.Machine, '') AS Search
